1614b20886deeddca50d92b70678a4fd22cfc90f
ViperEkura <3081035982@qq.com>
2025-07-04 18:10:50 +0800

    给出hpcg各个类初始化定义

--- include/hpcg.h
+++ include/hpcg.h
@@ -1,14 +1,38 @@
-class SpareseMatrix { 
+#include <map>
+#include <vector>
 
+class SpareseMatrix {
+private:
+    int rows;
+    int cols;
+    std::map<int64_t, float> data;
+
+public:
+    SpareseMatrix(int rows, int cols);
+    float& operator()(int row, int col);
+    float operator()(int row, int col) const;
+    size_t size() const;
 };
-
 
 class Vector {
+private:
+    std::vector<float> data;
 
+public:
+    Vector(int size);
+    float& operator[](int index);
+    float operator[](int index) const;
+    size_t size() const;
 };
 
-class ConjugateGradientSolver { 
+class ConjugateGradientSolver {
+private:
+    float tolerance;
+    int maxIter;
 
+public:
+    ConjugateGradientSolver(float tolerance, int maxIter);
+    Vector solve(const SpareseMatrix& A, const Vector& b) const;
 };
 
 
--- src/hpcg.cpp
+++ src/hpcg.cpp
@@ -1,1 +1,49 @@
 #include "hpcg.h"
+
+// SpareseMatrix
+SpareseMatrix::SpareseMatrix(int rows, int cols) :
+    rows(rows),
+    cols(cols){}
+
+float& SpareseMatrix::operator()(int row, int col){
+    int64_t index = (int64_t)(row) * cols + col;
+    return data[index];
+}
+
+float SpareseMatrix::operator()(int row, int col) const{
+    int64_t index = (int64_t)(row) * cols + col;
+    auto it = data.find(index);
+    if (it != data.end()) 
+        return it->second;
+    
+    return 0.0f; 
+}
+
+size_t SpareseMatrix::size() const{
+    return (size_t)(rows) * cols;
+}
+
+// Vector
+Vector::Vector(int size) :
+    data(std::vector<float>(size, 0.0f)){}
+
+float& Vector::operator[](int index){
+    return data[index];
+}
+float Vector::operator[](int index) const {
+    return data[index];
+}
+
+size_t Vector::size() const {
+    return data.size();
+}
+
+//ConjugateGradientSolver
+
+ConjugateGradientSolver::ConjugateGradientSolver(float tolerance, int maxIter) :
+    maxIter(maxIter),
+    tolerance(tolerance){}
+
+Vector ConjugateGradientSolver::solve(const SpareseMatrix& A, const Vector& b) const{
+     Vector x(b.size());
+}
